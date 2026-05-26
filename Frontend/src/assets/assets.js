e525614ee5983b62b7bbacace2713804e16dc635
user.name "avinash <avinash>
2026-05-27 01:51:46 +0530

    minor changes

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -14,7 +14,7 @@ import menu_8 from './menu_8.png'
 // ----------------------- BG -------------------------------------------------------
 
 import bg1 from './bg1.jpg'
-import bg2 from './bg2.jpg'
+// import bg2 from './bg2.jpg'
 import bg3 from './bg3.jpg'
 import bg4 from './bg4.jpg'
 import bg5 from './bg5.jpg'
@@ -90,7 +90,6 @@ export const assets = {
     bag_icon,
     parcel_icon,
     bg1,
-    bg2,
     bg3,
     bg4,
     bg5
